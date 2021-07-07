a06db718bc69885dbbcf63b2502ad8380e576015
jhonnybaltros <jhonnybaixo@gmail.com>
2021-07-07 19:22:17 -0300

    Appbar and PageHeader

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './App.css';
 import SideMenu from '../Components/SideMenu';
-import { CssBaseline, makeStyles } from '@material-ui/core';
+import { createMuiTheme, CssBaseline, makeStyles, ThemeProvider } from '@material-ui/core';
 import Header from '../Components/Header';
+import PageHeader from '../Components/PageHeader';
+import PeopleOutlineTwoToneIcon  from '@material-ui/icons/PeopleAltTwoTone';
+
+
+const theme = createMuiTheme({
+  palette:{
+    primary:{
+      main:"#333996",
+      light:"#3c44b126"
+  },
+  secondary:{
+    main:"#f83245",
+    light:"#f8324526"
+    },
+    background: {
+      default:"#f4f5fd"
+    }
+  }
+})
+
 
 const useStyles = makeStyles({
   appMain:{
@@ -15,13 +35,18 @@ function App() {
   const classes = useStyles();
   
   return (
-    <>
+    <ThemeProvider theme={theme}>
    <SideMenu />
     <div className={classes.appMain}>
       <Header />
+      <PageHeader
+      title="Page Header"
+      subtitle="Page description"
+      icon = {<PeopleOutlineTwoToneIcon fontSize="large" />}
+      />
     </div>
     <CssBaseline />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,28 +1,54 @@
-import { AppBar, Badge, Grid, InputBase, IconButton, Toolbar } from '@material-ui/core'
+import { AppBar, Badge, Grid, InputBase, IconButton, Toolbar, makeStyles } from '@material-ui/core'
 import React from 'react'
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone'
 import ChatBubbleOutlineIcon  from '@material-ui/icons/ChatBubbleOutline'
 import PowerSettingsNewIcon  from '@material-ui/icons/PowerSettingsNew'
+import SearchIcon  from '@material-ui/icons/Search'
 
+const useStyles = makeStyles(theme =>({
+    root: {
+        backgroundColor: '#fff',
+        transform:'translateZ(0)'
+    },
+    searchInput:{
+        opacity: '0.6',
+        padding: `0px ${theme.spacing(1)}px`, 
+        fontSize: '0.8rem',
+        '&:hover':{
+            backgroundColor:'#f2f2f2'
+        },
+        '& .MuiSvgIcon-root' :{
+            marginRight:theme.spacing(1)
+        },
+    }
+}))
 export default function Header() {
+
+    const classes = useStyles();
+
     return (
-       <AppBar position="static">
+       <AppBar position="static" className={classes.root}>
            
            <Toolbar>
-        <Grid container>
+        <Grid container
+        alignItems='center'>
             <Grid item> 
-                <InputBase />
+                <InputBase
+                placeholder="Search topics"
+                className={classes.searchInput}
+                startAdornment={<SearchIcon fonsize='small' />}
+                />
             </Grid>
             <Grid item sm></Grid>
             <Grid item> 
                 <IconButton>
                     <Badge badgeContent={4} color="secondary">
-                        <NotificationsNoneIcon />
+                        <NotificationsNoneIcon fonsize='small' />
                     </Badge>
                 </IconButton>
-                <IconButton>
+                <IconButton> 
                     <Badge badgeContent={3} color="primary">
-                        <ChatBubbleOutlineIcon />
+                        <ChatBubbleOutlineIcon fonsize='small' />
                     </Badge>
                 </IconButton>
                 <IconButton>
